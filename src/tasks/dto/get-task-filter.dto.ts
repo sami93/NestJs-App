8c78642b426269aa7d2f6bc27d5ebb31dca6ffd8
sami GHORBEL <sami.ghorbel@legalplace.fr>
2020-04-13 14:16:52 +0200

    add validation to filter

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,5 +1,11 @@
 import { TaskStatus } from '../task.model';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 export class GetTaskFilterDto {
+    @IsOptional()
+    @IsIn(Object.keys(TaskStatus).map(statut => TaskStatus[statut]))
     status: TaskStatus;
+
+    @IsOptional()
+    @IsNotEmpty()
     search: string
 }

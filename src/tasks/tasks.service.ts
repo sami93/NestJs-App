c0fd83faae6391d5259ac440c3e5368bc58695f2
sami GHORBEL <sami.ghorbel@legalplace.fr>
2020-04-13 11:22:07 +0200

    Adding Model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = []; // why private? any other component which inject this service can change array
+
+    getAllTasks(): Task[]{
+        return this.tasks
+    }
+}
